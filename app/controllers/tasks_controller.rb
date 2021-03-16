0b6eb1e08130347586c739a65b173485767ec708
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-03-16 02:07:43 +0100

    Task routes, and controller updates

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Tasks.all
+  end
+
+def show
+  @tasks = Tasks.find(params[:id])
+end
+
+def show
+  @tasks = Tasks.find(params[:id])
+end
+
+def new
+  @tasks = Tasks.new
+end
+
+
 end
